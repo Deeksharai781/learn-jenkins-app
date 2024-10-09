62abd9956bb5b4ea2f29a871c58b203161a5df1d
Deeksharai781 <51823649+Deeksharai781@users.noreply.github.com>
2024-10-09 14:10:42 +0000

    fix

--- e2e/app.spec.js
+++ e2e/app.spec.js
@@ -19,25 +19,9 @@ test('has expected app version', async ({ page }) => {
   await page.goto('/');
 
   const expectedAppVersion = process.env.REACT_APP_VERSION ? process.env.REACT_APP_VERSION : '1';
-  
-  console.log(`Expected App Version: ${expectedAppVersion}`);
 
-  // Wait for the element to be present
-  await page.waitForSelector(`p:has-text("Application version: ${expectedAppVersion}")`, { timeout: 5000 });
+  console.log(expectedAppVersion);
 
-  // Take a screenshot for debugging if needed
-  await page.screenshot({ path: 'screenshot.png' });
-
-  // Check for the existence of the element first
-  const elementCount = await page.locator(`p:has-text("Application version: ${expectedAppVersion}")`).count();
-  expect(elementCount).toBeGreaterThan(0); // Ensure the element exists
-
-  // Now check if the element is visible
-  const isVisible = await page.locator(`p:has-text("Application version: ${expectedAppVersion}")`).isVisible();
+  const isVisible = await page.locator(p:has-text("Application version: ${expectedAppVersion}")).isVisible();
   expect(isVisible).toBeTruthy();
-
-  // Optionally log the entire page content if needed for debugging
-  const pageContent = await page.content();
-  console.log(pageContent);
 });
-
