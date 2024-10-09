42c19422eacc2cf15ceb2491822c293a46c12fb0
Deeksharai781 <51823649+Deeksharai781@users.noreply.github.com>
2024-10-09 14:54:55 +0000

    fix

--- e2e/app.spec.js
+++ e2e/app.spec.js
@@ -22,6 +22,6 @@ test('has expected app version', async ({ page }) => {
 
   console.log(expectedAppVersion);
 
-  const isVisible = await page.locator(p:has-text("Application version: ${expectedAppVersion}")).isVisible();
+  const isVisible = await page.locator(`p:has-text("Application version: ${expectedAppVersion}")`).isVisible();
   expect(isVisible).toBeTruthy();
 });
